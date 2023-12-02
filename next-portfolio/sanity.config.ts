c534a2dd443815c2cc8a5a7386d25396e4105736
Josh Dale <joshuadale65@gmail.com>
2023-12-02 09:59:53 -0500

    project schemas added

--- next-portfolio/sanity.config.ts
+++ next-portfolio/sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
+import schemas from './sanity/schemas'
 
 const config = defineConfig({
     projectId: 'rjs62vz1',
@@ -7,7 +8,8 @@ const config = defineConfig({
     title: 'Portfolio Website',
     appVersion: '2023-11-19',
     basePath: '/studio',
-    plugins: [deskTool()]
+    plugins: [deskTool()],
+    schema: { types: schemas }
 })
 
 export default config
