e3bbc651959da5320578a5db5e180d9a2dff1a03
Josh Dale <joshuadale65@gmail.com>
2024-03-25 09:15:47 -0400

    page metadata

--- next-website/app/[...page]/page.tsx
+++ next-website/app/[...page]/page.tsx
@@ -1,29 +1,36 @@
 import { getPage } from "@/sanity/sanity-utils";
 import { PageContent } from "../Components/pageContent";
 import { headers } from 'next/headers';
+import Head from "next/head";
 
 type Props = {
     params: { page: string };
 };
 
-export function generateMetadata(page) {
-    console.log(page)
+export function generateMetadata(title: string) {
     return {
-        title: {page}
-    }
+        title: `Josh Dale | ${title}`
+    };
 }
-
 
 export default async function Page({ params }: Props) {
     const slug = params.page[0];
     const page = await getPage(slug);
     const header = headers();
     const previewData = header.previewData;
-    // generateMetadata({page})
-    generateMetadata({page.title})
-        console.log(page)
+    let metadata = null; // Initialize metadata variable
+
+    if (page.title) {
+        metadata = generateMetadata(page.title); // Create metadata object
+    }
     return (
         <>
+         {/* Set page title in the document head if metadata exists */}
+         {metadata && (
+                <head>
+                    <title>{metadata.title}</title>
+                </head>
+            )}
             <div className="text-2xl text-red-500">
                 {page?.title}
             </div>
